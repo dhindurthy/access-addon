4d37bd521ed3b71e1b5372657e55dfeeefda7a16
dhindurthy <dhirajindurthy@gmail.com>
2017-05-11 17:58:26 -0500

    Added display file for sort component

---
+++ tests/dummy/app/routes/aa-sort.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+const sortRoute = Ember.Route.extend({
+
+	actions: {
+
+		sortData: function() {
+			//alert('in the route');
+		}
+	}
+});
+
+export default sortRoute;
